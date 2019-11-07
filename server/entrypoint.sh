bfc254c4d035fb1e6d43a35b8e19ae91014dcd3a
Harry Jubb <harry.jubb@gmail.com>
2019-11-07 21:55:32 +0000

    Reorg and renaming for server services.

--- server/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-# Development server
-# FLASK_APP=server.py python -m flask run --host=0.0.0.0
-
-# Production gunicorn server
-gunicorn -b 0.0.0.0:5000 -w ${NUM_GUNICORN_WORKERS:-2} -k sync server:app
