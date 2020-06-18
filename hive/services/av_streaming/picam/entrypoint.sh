f0530a2bc36fe1584ed7ea393cd2c761b9e9b82e
Harry Jubb <harry.jubb@gmail.com>
2020-06-19 00:33:33 +0100

    Working HLS, testing TCP.

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-./picam --help
-sleep infinity
+# Symlink tmpfs (shm / RAM drive) to picam
+mkdir -p /shm/rec
+mkdir -p /shm/hooks
+mkdir -p /shm/state
+mkdir -p /shm/hls
+
+ln -sfn /picam/archive /shm/rec/archive
+ln -sfn /shm/rec /picam/rec
+ln -sfn /shm/hooks /picam/hooks
+ln -sfn /shm/state /picam/state
+ln -sfn /shm/hls /picam/hls
+
+# ./picam --alsadev $ALSA_DEVICE -o /shm/hls
+./picam --alsadev $ALSA_DEVICE --tcpout tcp://localhost:8181
+# ./picam --alsadev $ALSA_DEVICE -o /shm/hls --tcpout tcp://localhost:8181
