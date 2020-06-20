01f13f85da6a19431ae06567f8c73dc602a45158
Harry Jubb <harry.jubb@gmail.com>
2020-06-20 22:17:25 +0100

    #13 Hive-side STREAM_SECRET_KEY addition.

--- hive/services/av_streaming/ffmpeg/entrypoint.sh
+++ hive/services/av_streaming/ffmpeg/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -f flv $RTMP_URL/$STREAM_KEY
+ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
