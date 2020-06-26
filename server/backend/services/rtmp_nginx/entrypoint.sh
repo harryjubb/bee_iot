0f46813f208e632ffbf049754f21ab453e01d4a6
Harry Jubb <harry.jubb@gmail.com>
2020-06-26 23:23:56 +0000

    #16 Serve HLS stream
    Resolves #16.

--- server/backend/services/rtmp_nginx/entrypoint.sh
+++ server/backend/services/rtmp_nginx/entrypoint.sh
@@ -5,4 +5,7 @@ set -e
 export STREAM_SECRET_KEY
 envsubst '$STREAM_SECRET_KEY' < /templates/nginx.conf.template > /etc/nginx/nginx.conf
 
+mkdir -p /hlstmp/hls
+chmod -R 777 /hlstmp/hls
+
 nginx -g 'daemon off;'
