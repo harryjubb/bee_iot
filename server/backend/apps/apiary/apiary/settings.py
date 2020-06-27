0aba13ed75ccfa0190354da5b4bd1c5576827a18
Harry Jubb <harry.jubb@gmail.com>
2020-06-28 00:26:33 +0100

    CORS setup

--- server/backend/apps/apiary/apiary/settings.py
+++ server/backend/apps/apiary/apiary/settings.py
@@ -75,9 +75,11 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "graphene_django",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
