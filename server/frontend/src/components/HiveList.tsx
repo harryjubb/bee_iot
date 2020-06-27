8bb65afa90da3b85af344789abe8cc49b9629622
Harry Jubb <harry.jubb@gmail.com>
2020-06-28 00:28:43 +0100

    Basic frontend GraphQL setup

--- server/frontend/src/components/HiveList.tsx
+++ server/frontend/src/components/HiveList.tsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Typography } from "@material-ui/core";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+
+const HIVES = gql`
+  query {
+    allHives {
+      id
+      name
+      sponsor {
+        id
+        name
+        sponsorshipLevel
+      }
+    }
+  }
+`;
 
 export default function HiveList() {
+  const { loading, error, data } = useQuery(HIVES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
   return (
     <>
       <Typography variant="h2" gutterBottom>
         Hives
       </Typography>
+      {data.allHives.map((hive: any) => (
+        <div key={hive.id}>
+          {hive.name}
+          {hive.sponsor?.name}
+          {hive.sponsor?.sponsorshipLevel ?? "N/A"}
+        </div>
+      ))}
     </>
   );
 }
--- server/frontend/src/index.tsx
+++ server/frontend/src/index.tsx
@@ -4,9 +4,18 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "@apollo/react-hooks";
+
+const client = new ApolloClient({
+  uri: "http://localhost:8000/graphql",
+});
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
