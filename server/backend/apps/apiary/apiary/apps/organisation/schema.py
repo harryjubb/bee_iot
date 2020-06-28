dfd5cb06ae6de255302ca8e3b18129816752b755
Harry Jubb <harry.jubb@gmail.com>
2020-06-28 22:34:40 +0100

    Custom resolver for organisaton logo absolute URLs

--- server/backend/apps/apiary/apiary/apps/organisation/schema.py
+++ server/backend/apps/apiary/apiary/apps/organisation/schema.py
@@ -7,6 +7,11 @@ class OrganisationType(DjangoObjectType):
     class Meta:
         model = Organisation
 
+    def resolve_logo(self, info):
+        if self.logo:
+            return info.context.build_absolute_uri(self.logo.url) if self.logo else None
+        return None
+
 
 class Query(object):
     pass
