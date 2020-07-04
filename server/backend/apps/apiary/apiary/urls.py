9ba314a2cd125f4a5438c7cdbefde8485eede4d7
Harry Jubb <harry.jubb@gmail.com>
2020-07-04 23:57:37 +0100

    Admin titles

--- server/backend/apps/apiary/apiary/urls.py
+++ server/backend/apps/apiary/apiary/urls.py
@@ -21,6 +21,10 @@ from django.conf import settings
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
+admin.site.title = "Apiary Administration"
+admin.site.site_header = "Apiary Administration"
+admin.site.index_title = "Apiary Administration"
+
 urlpatterns = [path("admin/", admin.site.urls),] + static(
     settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
 )
