c7d483b4c653ca20f7e7e18ee428d0a89f30081c
Harry Jubb <harry.jubb@gmail.com>
2020-07-05 20:14:04 +0100

    Production Apiary app config

--- server/backend/apps/apiary/entrypoint.production.sh
+++ server/backend/apps/apiary/entrypoint.production.sh
@@ -1,1 +1,12 @@
 #!/bin/sh
+
+set -e
+
+python manage.py migrate
+
+# Set up a superuser if one doesn't already exist
+echo "Adding a preconfigured superuser if one doesn't exist"
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('$APIARY_SUPERUSER_USERNAME', '', '$APIARY_SUPERUSER_PASSWORD') if not User.objects.filter(username='$APIARY_SUPERUSER_USERNAME').count() else 1" | python manage.py shell
+
+echo "Running in production with gunicorn"
+gunicorn --workers="$APIARY_NUM_GUNICORN_WORKERS" -b 0.0.0.0:8000 'apiary.wsgi'
