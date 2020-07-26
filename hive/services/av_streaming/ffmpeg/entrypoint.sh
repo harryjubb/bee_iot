ece43df12fd3936ad431ebc965cc34b571aebe7a
Harry Jubb <harry.jubb@gmail.com>
2020-07-26 12:30:34 +0100

    ffmpeg reconnect

--- hive/services/av_streaming/ffmpeg/entrypoint.sh
+++ hive/services/av_streaming/ffmpeg/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
+ffmpeg -reconnect 1 -reconnect_at_eof 1 -reconnect_streamed 1 -reconnect_delay_max 2 -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
