1ec067d8b9742f6da170e55e9289ef8884002317
Harry Jubb <harry.jubb@gmail.com>
2021-06-19 23:44:07 +0100

    Quote echos

--- hive/services/av_streaming/ffmpeg/entrypoint.sh
+++ hive/services/av_streaming/ffmpeg/entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-echo *** Initial ffmpeg start
+echo '*** Initial ffmpeg start'
 
 # https://github.com/iizukanao/picam/issues/113#issuecomment-787083762
 while true; do
   if ! pgrep ffmpeg > /dev/null ; then
     # ffmpeg processes were killed
-    echo *** Restarting ffmpeg...
+    echo '*** Restarting ffmpeg...'
     sleep 2
     # Start processes again
     ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -timeout 5 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
