d28ad27c350bca5bf5e6245d7c0f1a323912f372
Harry Jubb <harry.jubb@gmail.com>
2021-06-19 21:17:21 +0100

    Fix app names

--- server/backend/apiary/apiary/apps/hive/apps.py
+++ server/backend/apiary/apiary/apps/hive/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class HiveConfig(AppConfig):
-    name = 'hive'
+    name = 'apiary.apps.hive'
--- server/backend/apiary/apiary/apps/organisation/apps.py
+++ server/backend/apiary/apiary/apps/organisation/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class OrganisationConfig(AppConfig):
-    name = "organisation"
+    name = "apiary.apps.organisation"
