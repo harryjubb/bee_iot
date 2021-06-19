3efd5d2cc2b3f2eb388295d418b1bd8cf73f4684
Harry Jubb <harry.jubb@gmail.com>
2021-06-20 00:57:19 +0100

    Fudge delay to wait for ffmpeg startup

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -12,7 +12,7 @@ ln -sfn /shm/hooks /picam/hooks
 ln -sfn /shm/state /picam/state
 ln -sfn /shm/hls /picam/hls
 
-echo '*** Picam start'
+echo '*** Picam container loop start'
 
 # https://github.com/iizukanao/picam/issues/113#issuecomment-787083762
 while true; do
@@ -20,7 +20,11 @@ while true; do
     # Kill picam and ffmpeg processes
     sleep 2
     # Start processes again
+    echo '*** Starting ffmpeg'
     ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
+    # Fudge delay for ffmpeg to spin up before starting picam
+    sleep 5
+    echo '*** Starting picam'
     ./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://127.0.0.1:8181
   fi
   sleep 2
