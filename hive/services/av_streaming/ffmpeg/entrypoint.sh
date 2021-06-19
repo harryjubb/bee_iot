43296e681b7ed28f62b13a557a3b86e3344c0d61
Harry Jubb <harry.jubb@gmail.com>
2021-06-19 23:55:42 +0100

    Try another ffmpeg timeout option

--- hive/services/av_streaming/ffmpeg/entrypoint.sh
+++ hive/services/av_streaming/ffmpeg/entrypoint.sh
@@ -9,7 +9,7 @@ while true; do
     echo '*** Restarting ffmpeg...'
     sleep 2
     # Start processes again
-    ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -timeout 5 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
+    ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000  -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
   fi
   sleep 5
 done
