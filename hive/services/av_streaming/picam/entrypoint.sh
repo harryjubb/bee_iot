9f8d1ad0cb1fd5fb64c78490e6c8664442431a9f
Harry Jubb <harry.jubb@gmail.com>
2021-06-20 00:50:30 +0100

    Try running ffmpeg in the picam container

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -13,4 +13,15 @@ ln -sfn /shm/state /picam/state
 ln -sfn /shm/hls /picam/hls
 
 echo '*** Picam start'
-./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://av_streaming_ffmpeg:8181
+
+# https://github.com/iizukanao/picam/issues/113#issuecomment-787083762
+while true; do
+  if ! pgrep ffmpeg > /dev/null ; then
+    # Kill picam and ffmpeg processes
+    sleep 2
+    # Start processes again
+    ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
+    ./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://127.0.0.1:8181
+  fi
+  sleep 2
+done
