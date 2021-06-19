4b31140483292b67756f286d5d770b95f7ffa300
Harry Jubb <harry.jubb@gmail.com>
2021-06-20 00:16:15 +0100

    Revert ffmpeg timeout argument

--- hive/services/av_streaming/ffmpeg/entrypoint.sh
+++ hive/services/av_streaming/ffmpeg/entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo '*** ffmpeg start'
-ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -timeout 5 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
+ffmpeg -i tcp://av_streaming_ffmpeg:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY
