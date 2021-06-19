a33f78a26b406be3da4f162571b7bc6bc17c0e4f
Harry Jubb <harry.jubb@gmail.com>
2021-06-19 23:51:19 +0100

    Restart logic for picam

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -12,4 +12,14 @@ ln -sfn /shm/hooks /picam/hooks
 ln -sfn /shm/state /picam/state
 ln -sfn /shm/hls /picam/hls
 
-./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://av_streaming_ffmpeg:8181  
+# https://github.com/iizukanao/picam/issues/113#issuecomment-787083762
+while true; do
+  if ! pgrep picam > /dev/null ; then
+    # ffmpeg processes were killed
+    echo '*** Restarting picam...'
+    sleep 2
+    # Start processes again
+    ./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://av_streaming_ffmpeg:8181
+  fi
+  sleep 5
+done
