716daefb22297fb0574a3de5cf93b70b78131084
Harry Jubb <harry.jubb@gmail.com>
2021-06-20 01:47:02 +0100

    Temp remove kill from healthcheck

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -12,7 +12,7 @@ ln -sfn /shm/hooks /picam/hooks
 ln -sfn /shm/state /picam/state
 ln -sfn /shm/hls /picam/hls
 
-echo '*** Starting ffmpeg'
+echo '*** Starting ffmpeg in the background'
 ./ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
 # Fudge delay for ffmpeg to spin up before starting picam
 sleep 5
