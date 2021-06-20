c4f665a36a48713ffe3eaa8c4f1e1403a6492e7d
Harry Jubb <harry.jubb@gmail.com>
2021-06-20 01:39:41 +0100

    Try to use ffmpeg running state to kill the container via the healthcheck

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -12,20 +12,9 @@ ln -sfn /shm/hooks /picam/hooks
 ln -sfn /shm/state /picam/state
 ln -sfn /shm/hls /picam/hls
 
-echo '*** Picam container loop start'
-
-# https://github.com/iizukanao/picam/issues/113#issuecomment-787083762
-while true; do
-  if ! pgrep ffmpeg > /dev/null ; then
-    # Kill picam and ffmpeg processes
-    sleep 2
-    # Start processes again
-    echo '*** Starting ffmpeg'
-    ./ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
-    # Fudge delay for ffmpeg to spin up before starting picam
-    sleep 5
-    echo '*** Starting picam'
-    ./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://127.0.0.1:8181
-  fi
-  sleep 2
-done
+echo '*** Starting ffmpeg'
+./ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
+# Fudge delay for ffmpeg to spin up before starting picam
+sleep 5
+echo '*** Starting picam'
+./picam --autoex --time --alsadev $ALSA_DEVICE -c $AUDIO_CHANNELS --tcpout tcp://127.0.0.1:8181
