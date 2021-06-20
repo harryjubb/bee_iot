d4cd4d2be46671b1d50507fe2666ed38a6a295f6
Harry Jubb <harry.jubb@gmail.com>
2021-06-20 01:27:31 +0100

    Update ffmpeg path

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -21,7 +21,7 @@ while true; do
     sleep 2
     # Start processes again
     echo '*** Starting ffmpeg'
-    ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
+    ./ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
     # Fudge delay for ffmpeg to spin up before starting picam
     sleep 5
     echo '*** Starting picam'
