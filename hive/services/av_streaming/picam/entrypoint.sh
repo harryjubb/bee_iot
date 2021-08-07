e71e2c95675f9db84a0c787ce873ce86c0428349
Harry Jubb <harry.jubb@gmail.com>
2021-08-07 10:33:11 +0100

    Switch picam to distinct tmpfs definition

--- hive/services/av_streaming/picam/entrypoint.sh
+++ hive/services/av_streaming/picam/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-# Symlink tmpfs (shm / RAM drive) to picam
-mkdir -p /shm/rec
-mkdir -p /shm/hooks
-mkdir -p /shm/state
-mkdir -p /shm/hls
+# Symlink tmpfs (RAM mount) to picam
+mkdir -p /picam_tmpfs/rec
+mkdir -p /picam_tmpfs/hooks
+mkdir -p /picam_tmpfs/state
+mkdir -p /picam_tmpfs/hls
 
-ln -sfn /picam/archive /shm/rec/archive
-ln -sfn /shm/rec /picam/rec
-ln -sfn /shm/hooks /picam/hooks
-ln -sfn /shm/state /picam/state
-ln -sfn /shm/hls /picam/hls
+ln -sfn /picam/archive /picam_tmpfs/rec/archive
+ln -sfn /picam_tmpfs/rec /picam/rec
+ln -sfn /picam_tmpfs/hooks /picam/hooks
+ln -sfn /picam_tmpfs/state /picam/state
+ln -sfn /picam_tmpfs/hls /picam/hls
 
 echo '*** Starting ffmpeg in the background'
 ./ffmpeg -i tcp://127.0.0.1:8181?listen -c:v copy -c:a aac -strict -2 -ar 44100 -ab 40000 -rw_timeout 5000000 -f flv $RTMP_URL/$STREAM_KEY?psk=$STREAM_SECRET_KEY &
