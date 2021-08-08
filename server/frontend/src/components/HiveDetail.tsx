15a121ecdea76dd09343321fdfb2ecf2900a7aba
Harry Jubb <harry.jubb@gmail.com>
2021-08-08 11:34:15 +0100

    Dashboard embed styles

--- server/frontend/src/components/HiveDetail.tsx
+++ server/frontend/src/components/HiveDetail.tsx
@@ -166,7 +166,7 @@ export default function HiveDetail() {
           <Typography variant="h4" gutterBottom>
             Sensor data
           </Typography>
-          <iframe title={`${hive.name} sensor data`} width="100%" height="500px" src={hive.dashboardUrl}></iframe>
+          <iframe title={`${hive.name} sensor data`} style={{width: '100%', height: '50vh', border: 'none'}} src={hive.dashboardUrl}></iframe>
         </> : <></>
       }
     </>
